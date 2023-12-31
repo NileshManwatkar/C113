a1439d628bf01acddb6bbd1507dd3512044379ec
NileshManwatkar <152001212+NileshManwatkar@users.noreply.github.com>
2023-12-31 11:41:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png' ,
+    png_quality:90
+});
+
+caches,era = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" scr="'+data_uri+'"/>"';
+    });
+}
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/RlzytX9xd/', modelLoaded);
+
+function cheack() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("resuult_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
